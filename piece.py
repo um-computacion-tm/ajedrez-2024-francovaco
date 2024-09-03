65d3b9026f67b08ceec9b5c53a3af6f6b1d958a5
Franco Vaccarezza <vaco@MacBook-Pro-Vaco.local>
2024-09-03 10:45:35 -0300

    correccion duplicacion

--- piece.py
+++ piece.py
@@ -19,3 +19,27 @@ class Piece:
         Retorna el valor del atributo __color__.
         '''
         return self.__color__
+    
+    def possible_moves_general(self, from_row, from_col, directions):
+        '''
+        La función retorna una lista con las posiciones a las que el rey puede moverse.
+        Funcionamiento:
+        Se crea una lista vacía llamada moves.
+        Se crea una lista llamada directions con las posiciones a las que el rey puede moverse.
+        Se recorre la lista directions.
+        Se crea una variable new_row que almacena la suma de la posición de la fila actual y la fila de la dirección.
+        Se crea una variable new_col que almacena la suma de la posición de la columna actual y la columna de la dirección.
+        Se verifica si la nueva posición está dentro del tablero.
+        Se agrega la nueva posición a la lista moves.
+        Parámetros:
+        from_row: Recibe la fila de la posición actual del rey.
+        from_col: Recibe la columna de la posición actual.
+        '''
+        moves = []
+
+        for direction in directions:
+            new_row = from_row + direction[0]
+            new_col = from_col + direction[1]      
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
+                moves.append((new_row, new_col))          
+        return moves
