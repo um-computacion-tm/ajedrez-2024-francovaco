f0b3972dfb346eb46d301b3bf24e88c790852b5e
Franco Vaccarezza <vaco@MacBook-Pro-Vaco.local>
2024-09-05 19:36:07 -0300

    correccion codeclimate bishop y queen

--- test_king.py
+++ test_king.py
@@ -12,16 +12,5 @@ class TestKing(unittest.TestCase):
         self.assertEqual(str(king_white), 'K')
         self.assertEqual(str(king_black), 'k')
 
-    def test_possible_moves(self):
-        '''
-        La función test_possible_moves es una prueba unitaria que verifica los movimientos posibles de un rey desde el centro del tablero.
-        '''
-        king = King('WHITE')
-        expected_moves = [
-            (2, 2), (2, 4), (4, 2), (4, 4), # Diagonales
-            (2, 3), (4, 3), (3, 2), (3, 4) # Laterales
-        ]
-        self.assertEqual(king.possible_moves(3, 3), expected_moves)
-
 if __name__ == '__main__':
     unittest.main()
